664a2a5def83df98f7a1eec523bc8bba8ab4d736
paullcv <paul.cruz.4.pc@gmail.com>
2023-11-12 02:08:27 -0400

    Plantilla adaptada

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,82 @@
-@extends('layouts.app')
+@extends('layouts.form')
+
+@section('title','Registrate')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
+        <!-- Table -->
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-8">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        @if ($errors->any())
+                            <div class="text-center text-muted mb-2">
+                                <h4>Se encontro el siguiente error.</h4>
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="alert alert-danger mb-4" role="alert">
+                                {{ $errors->first() }}
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        @else
+                            <div class="text-center text-muted mb-4">
+                                <small>Introduce tus datos</small>
                             </div>
-                        </div>
+                        @endif
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                        {{-- Form --}}
+                        <form role="form" method="POST" action="{{ route('register') }}">
+                          @csrf
+                            {{-- Nombre --}}
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                                    </div>
+                                    <input class="form-control" name="name" value="{{ old('name') }}" required
+                                        autocomplete="name" autofocus placeholder="Nombre" type="text">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                            {{-- Email --}}
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="Correo Electronico" type="email"
+                                        name="email" value="{{ old('email') }}" required autocomplete="email">
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            {{-- Password --}}
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    </div>
+                                    <input class="form-control" name="password" required autocomplete="new-password"  placeholder="Contraseña" type="password">
+                                </div>
+                            </div>
+
+                            {{-- Confirm Password --}}
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    </div>
+                                    <input class="form-control" name="password_confirmation" required
+                                        autocomplete="new-password" placeholder="Confirmar Contraseña" type="password">
+                                </div>
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary mt-4">Registrarse</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/includes/panel/menu.blade.php
@@ -0,0 +1,68 @@
+<h6 class="navbar-heading text-muted">Gestion</h6>
+<ul class="navbar-nav">
+    <li class="nav-item  active ">
+        <a class="nav-link  active " href="{{ url('/home') }}">
+            <i class="ni ni-tv-2 text-danger"></i> Dashboard
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="{{ url('/especialidades') }}">
+            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Empleados
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="./examples/maps.html">
+            <i class="ni ni-tv-2 text-danger"></i> Roles y Permisos
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " href="./examples/profile.html">
+            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar CV
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link " href="./examples/profile.html">
+            <i class="fas fa-procedures text-warning"></i> Asistencia
+        </a>
+    </li>
+  
+    {{-- <li class="nav-item">
+        <a class="nav-link " href="./examples/profile.html">
+            <i class="ni ni-briefcase-24 text-blue"></i> Rendimiento
+        </a>
+    </li>
+  
+    <li class="nav-item">
+        <a class="nav-link " href="./examples/profile.html">
+            <i class="fas fa-procedures text-warning"></i> Opcion
+        </a>
+    </li> --}}
+  
+  
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
+            <i class="fas fa-sign-in-alt"></i> Cerrar sesión
+        </a>
+        <form action="{{ route('logout') }}" method="POST" style="display: none;" id="formLogout">
+            @csrf
+        </form>
+    </li>
+</ul>
+<!-- Divider -->
+<hr class="my-3">
+<!-- Heading -->
+<h6 class="navbar-heading text-muted">Reportes</h6>
+<!-- Navigation -->
+<ul class="navbar-nav mb-md-3">
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="far fa-calendar-alt text-info"></i> Reportes Ventas
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="far fa-chart-bar text-blue"></i> Reportes Ganancias
+        </a>
+    </li>
