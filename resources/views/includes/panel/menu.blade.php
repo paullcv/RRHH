77c6351cebcc5863b13a7441223e1a4020041fc1
melanieyl <melanieyupanqui44@gmail.com>
2023-11-26 11:20:12 -0300

    vista de roles, y roleas aplicados al menu

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -1,67 +1,80 @@
 <h6 class="navbar-heading text-muted">Gestion</h6>
 <ul class="navbar-nav">
-    <li class="nav-item  active ">
-        <a class="nav-link  active " href="{{ url('/home') }}">
+    @can('Administrador')
+    <li class="nav-item">
+        <a class="nav-link active" href="{{ url('/home') }}">
             <i class="ni ni-tv-2 text-danger"></i> Dashboard
         </a>
     </li>
+    @endcan
+    @can('Administrador')
+    <li class="nav-item">
+        <a class="nav-link" href="{{ url('/empleados') }}">
+            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Empleados
+        </a>
+    </li>
+    @endcan
+    @can('Administrador')
+    <li class="nav-item">
+        <a class="nav-link" href="{{ url('/roles') }}">
+            <i class="ni ni-tv-2 text-danger"></i> Roles y Permisos
+        </a>
+    </li>
+    @endcan
+    @can('Administrador')
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/postulantes') }}">
             <i class="ni ni-briefcase-24 text-blue"></i>Gestionar Postulantes
         </a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link " href="#">
-            <i class="ni ni-tv-2 text-danger"></i> Roles y Permisos
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link " href="{{ url('/empleados') }}">
-            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Empleados
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link " href="{{ url('/asistencias') }}">
-            <i class="fas fa-procedures text-warning"></i> Gestionar Asistencias
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link " href="{{ url('/horarios') }}">
-            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Horarios
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link " href="{{ url('/jornadas') }}">
-            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Jornadas
-        </a>
-    </li>
-
+    @endcan
+    @can('Administrador')
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/departments') }}">
             <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Departamentos
         </a>
     </li>
-
+    @endcan
+    @can('Administrador')
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/cargos') }}">
             <i class="fas fa-procedures text-warning"></i> Gestionar Cargos
         </a>
     </li>
+    @endcan
+    @can('Administrador')
+    <li class="nav-item">
+        <a class="nav-link " href="{{ url('/asistencias') }}">
+            <i class="fas fa-procedures text-warning"></i> Gestionar Asistencias
+        </a>
+    </li>
+    @endcan
+    @can('Administrador')
+    <li class="nav-item">
+        <a class="nav-link " href="{{ url('/horarios') }}">
+            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Horarios
+        </a>
+    </li>
+    @endcan
+    @can('Administrador')
+    <li class="nav-item">
+        <a class="nav-link " href="{{ url('/jornadas') }}">
+            <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Jornadas
+        </a>
+    </li>
+    @endcan
+    @if(auth()->user()->hasAnyRole(['Administrador', 'Empleado']))
     <li class="nav-item">
         <a class="nav-link" href="{{ url('/misasistencias') }}">
             <i class="fas fa-procedures text-warning"></i> Mis asistencias
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link " href="{{ url('/misNominas') }}">
+        <a class="nav-link" href="{{ url('/misNominas') }}">
             <i class="fas fa-procedures text-warning"></i> Mis nominas
         </a>
     </li>
-
-
+    @endif
     <li class="nav-item">
         <a class="nav-link" href="{{ route('logout') }}"
             onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
