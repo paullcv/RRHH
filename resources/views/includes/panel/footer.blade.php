6355f1e59a6365e57905e2f4f1749d0745ad5cca
melanieyl <melanieyupanqui44@gmail.com>
2023-11-26 11:20:12 -0300

    vista de roles, y roleas aplicados al menu

--- resources/views/includes/panel/footer.blade.php
+++ resources/views/includes/panel/footer.blade.php
@@ -8,7 +8,7 @@
         <div class="col-xl-6">
             <ul class="nav nav-footer justify-content-center justify-content-xl-end">
                 <li class="nav-item">
-                    <a href="#" class="nav-link">¿Quienes somos?</a>
+                    <a href="{{ url('/') }}" class="nav-link">¿Quienes somos?</a>
                 </li>
             </ul>
         </div>
