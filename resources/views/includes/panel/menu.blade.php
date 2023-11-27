273fb3b3659a918039228de566d70433b1d91211
melanieyl <melanieyupanqui44@gmail.com>
2023-11-27 17:56:56 -0300

    correccion en menu

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -1,68 +1,69 @@
 <h6 class="navbar-heading text-muted">Gestion</h6>
 <ul class="navbar-nav">
-    @can('Administrador')
+    
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link active" href="{{ url('/home') }}">
             <i class="ni ni-tv-2 text-danger"></i> Dashboard
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link" href="{{ url('/empleados') }}">
             <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Empleados
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link" href="{{ url('/roles') }}">
             <i class="ni ni-tv-2 text-danger"></i> Roles y Permisos
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/postulantes') }}">
             <i class="ni ni-briefcase-24 text-blue"></i>Gestionar Postulantes
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/departments') }}">
             <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Departamentos
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/cargos') }}">
             <i class="fas fa-procedures text-warning"></i> Gestionar Cargos
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/asistencias') }}">
             <i class="fas fa-procedures text-warning"></i> Gestionar Asistencias
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/horarios') }}">
             <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Horarios
         </a>
     </li>
-    @endcan
-    @can('Administrador')
+    @endif
+    @if(auth()->user()->hasRole('Administrador'))
     <li class="nav-item">
         <a class="nav-link " href="{{ url('/jornadas') }}">
             <i class="ni ni-briefcase-24 text-blue"></i> Gestionar Jornadas
         </a>
     </li>
-    @endcan
+    @endif
     @if(auth()->user()->hasAnyRole(['Administrador', 'Empleado']))
     <li class="nav-item">
         <a class="nav-link" href="{{ url('/misasistencias') }}">
