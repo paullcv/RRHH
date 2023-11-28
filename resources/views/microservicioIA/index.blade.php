fac72388c55e0062af198a01521987d457cebe07
melanieyl <melanieyupanqui44@gmail.com>
2023-11-28 15:46:46 -0300

    api enviar y get analisis

---
+++ resources/views/microservicioIA/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.panel')
+
+@section('content')
+    <div class="card shadow">
+        <div class="card-header border-0">
+            <div class="row align-items-center">
+                <div class="col">
+                    <h3 class="mb-0">Lista de Postulantes</h3>
+              {{datos}}
+            </div>
+        </div>
+        <div class="card-body">
+          
+        </div>
+
+    </div>
+@endsection
